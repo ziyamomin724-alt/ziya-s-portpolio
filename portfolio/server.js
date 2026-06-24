cbb83770a59aaf0d9349b99f8a40807be0eb7276
ziyamomin724-alt <ziyamomin724@gmail.com>
2026-06-24 20:49:55 +0530

    new

--- portfolio/server.js
+++ portfolio/server.js
@@ -17,6 +17,10 @@ app.get('/', (req, res) => {
 });
 
 // Start Server
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
+}
+
+module.exports = app;
